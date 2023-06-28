722d6fe943a431156b797594a6874a60f3f8c27f
cindygv <cindygracyavcgv@gmail.com>
2023-06-28 23:30:44 +0700

    Commit 1 cindy

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('employees')->insert([
+            [
+                'firstname' => 'Cindy',
+                'lastname' => 'Gracya',
+                'email' => 'cindygracyavcgv@gmail.com',
+                'age' => 19,
+                'position_id' => 1
+            ],
+            [
+                'firstname' => 'Siuchon',
+                'lastname' => 'Chan',
+                'email' => 'siuchonchan@gmail.com',
+                'age' => 30,
+                'position_id' => 2
+            ],
+            [
+                'firstname' => 'Cookies',
+                'lastname' => 'Oreo',
+                'email' => 'cookiesoreo@gmail.com',
+                'age' => 27,
+                'position_id' => 3
+            ],
+        ]);
+    }
+}
